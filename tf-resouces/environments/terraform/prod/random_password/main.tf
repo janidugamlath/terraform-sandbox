2897350d7d91870250f56c7606aa8476bfda3a9f
janidugamlath <janidu.gamlath@prodoscore.com>
2026-04-20 22:54:07 +0530

    update prod random password with indentation issue

--- tf-resouces/environments/terraform/prod/random_password/main.tf
+++ tf-resouces/environments/terraform/prod/random_password/main.tf
@@ -1,10 +1,10 @@
 resource "random_pet" "db_name" {
-  prefix = var.pet_prefix
-  length = 2
+  prefix= var.pet_prefix
+  length =2
 }
 
 resource "random_password" "password" {
-  length           = var.password_length
-  special          = true
+  length = var.password_length
+  special = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
