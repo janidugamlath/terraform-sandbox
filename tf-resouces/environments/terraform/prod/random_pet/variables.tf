47806f80700f6ff6a5c00449fc3731e44fc0afc8
janidugamlath <janidu.gamlath@prodoscore.com>
2026-04-20 18:35:02 +0530

    changes added to test workflow

--- tf-resouces/environments/terraform/prod/random_pet/variables.tf
+++ tf-resouces/environments/terraform/prod/random_pet/variables.tf
@@ -1,7 +1,6 @@
 variable "ami_id" {
   type        = string
-  default     = "test-id7"
+  default     = "test-id1"
   description = "The ID used to trigger a name change"
 }
 
-#check direct push test comment again again
