8fa44a25bdfe068922d9bbb15a80dac6db780998
janidugamlath <273011466+janidugamlath@users.noreply.github.com>
2026-04-21 04:32:50 +0000

    style: automated terraform fmt

--- tf-resouces/environments/terraform/prod/random_pet/variables.tf
+++ tf-resouces/environments/terraform/prod/random_pet/variables.tf
@@ -1,5 +1,5 @@
 variable "ami_id" {
   type        = string
-  default="test-id5"
-  description= "The ID used to trigger a name change"
+  default     = "test-id5"
+  description = "The ID used to trigger a name change"
 }
