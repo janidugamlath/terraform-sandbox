4f886d76def74111e1a95cc750167e551e6efaf5
janidugamlath <273011466+janidugamlath@users.noreply.github.com>
2026-04-21 10:24:37 +0000

    style: automated terraform fmt

--- tf-resouces/environments/terraform/dev/random_password_dev/main.tf
+++ tf-resouces/environments/terraform/dev/random_password_dev/main.tf
@@ -1,12 +1,12 @@
 resource "random_pet" "db_name" {
   prefix = var.pet_prefix
-  length=2
+  length = 2
 }
 
 resource "random_password" "password" {
-  length=var.password_length
-  special=true
-  override_special=           "!#$%&*()-_=+[]{}<>:?"
+  length           = var.password_length
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
 
