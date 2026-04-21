58c1575e38460e933779cd9faff8d6548312681b
janidugamlath <janidu.gamlath@prodoscore.com>
2026-04-21 10:18:21 +0530

    Update variables.tf

--- tf-resouces/environments/terraform/dev/random_password_dev/variables.tf
+++ tf-resouces/environments/terraform/dev/random_password_dev/variables.tf
@@ -1,11 +1,11 @@
 variable "password_length" {
-  type    = number
-  default = 16
+  type= number
+  default =16
 }
 
 variable "pet_prefix" {
-  type    = string
-  default = "databasetest1"
+  type=string
+  default= "databasetest1"
 }
 
 
