19a426bd6a812f3100f8d9155a3662e6c4535af2
janidugamlath <janidu.gamlath@prodoscore.com>
2026-04-28 11:09:13 +0530

    Update main.tf

--- environments/dev/random_password_dev/main.tf
+++ environments/dev/random_password_dev/main.tf
@@ -3,6 +3,11 @@ resource "random_pet" "db_name" {
   length = 3
 }
 
+resource "random_pet" "db_name1" {
+  prefix = var.pet_prefix
+  length = 3
+}
+
 resource "random_password" "password" {
   length           = var.password_length
   special          = true
